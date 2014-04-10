e63e65496d72816a89694ffa3332c6a9b60ea044
Olivia Hooks <ohooks@chicagotechacademy.org>
2014-04-10 13:15:43 -0500

    addes routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 ThirdApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
 
-  get "static_pages/about"
+match '/help', to: 'static_pages#help' #this creates route help_path
+match '/about', to: 'static_pages#about' #this creates route about_path
+match '/contacy', to: 'static_pages#contact' #this creates route contact_path
+
+root to: => 'static_pages#home'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
