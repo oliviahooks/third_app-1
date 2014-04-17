8417542a0abdbf990f06943477d15c1c172901c0
Olivia Hooks <ohooks@chicagotechacademy.org>
2014-04-17 13:13:43 -0500

    fixed app and static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,24 +16,28 @@ describe "StaticPages" do
     end
   end
     describe "Help page" do
+
+    before {visit help_path}
+
     it "should have the content Help Page" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit  help_path
+      
       page.should have_selector("h1", text: 'Help Page')
     end
     it "should have a title" do
-      visit  help_path
+     
       page.should have_selector("title", text: 'Help Page')
     end
   end
   describe "About Us page" do
+    before{visit about_path}
     it "should have the content About Us" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit  about_path
+      
       page.should have_selector("h1", text: 'About Us')
     end
     it "should have a title" do
-      visit  about_path
+      
       page.should have_selector("title", text: 'About Page')
     end
   end
