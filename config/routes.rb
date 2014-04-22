0654b86768abde17ab882a81d66fbb0d4c719bb6
Olivia Hooks <ohooks@chicagotechacademy.org>
2014-04-22 13:19:03 -0500

    Added user pages
    Video 11 / 11:49 mins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ThirdApp::Application.routes.draw do
 
 
+ 
+  match '/signup', to: 'users#new'
+
 match '/help', to: 'static_pages#help' #this creates route help_path
 match '/about', to: 'static_pages#about' #this creates route about_path
 match '/contact', to: 'static_pages#contact' #this creates route contact_path
@@ -29,7 +32,7 @@ root :to => 'static_pages#home'
   #       post 'toggle'
   #     end
   #
-  #     collection do
+#     collection do
   #       get 'sold'
   #     end
   #   end
